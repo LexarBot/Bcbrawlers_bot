1515029bfe25708260b8467d1d88011f19d9a3b8
LexarBot <88177221+LexarBot@users.noreply.github.com>
2022-04-01 22:26:14 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+for (var i=0;i<9999;i++)
+{ 
+    setTimeout(function(){const mapBtn = document.querySelector("#root > div > div.sc-KfMfS.grokbn > div.sc-ikZpkk.cphgLt > div.sc-gXmSlM.hDiqvF > div.sc-iAvgwm.blYeqE > button.sc-dkzDqf.bxvWp > div");mapBtn.click();setTimeout(function(){const map2Btn = document.querySelector("#root > div > div.sc-KfMfS.grokbn > div.sc-ksZaOG.kKcEzM > div > div.sc-fHsOPI.hNiYiO > button > div");map2Btn.click();},30000); },3600000);
+}
